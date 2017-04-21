8d5b6e892fe16dda393c021767a346e966f97ee3
phantamanta44 <evandalong@gmail.com>
2017-04-21 18:41:06 -0500

    Finish tool implementation and add recipes

--- src/main/java/io/github/phantamanta44/mcrail/railtech/tool/ToolDamageHandler.java
+++ src/main/java/io/github/phantamanta44/mcrail/railtech/tool/ToolDamageHandler.java
@@ -1,9 +1,11 @@
 package io.github.phantamanta44.mcrail.railtech.tool;
 
 import io.github.phantamanta44.mcrail.Rail;
-import io.github.phantamanta44.mcrail.railtech.IEnergyProvider;
+import io.github.phantamanta44.mcrail.railflux.IEnergyProvider;
 import io.github.phantamanta44.mcrail.railtech.util.Hardness;
 import io.github.phantamanta44.mcrail.util.AdapterUtils;
+import io.github.phantamanta44.mcrail.util.ItemUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -25,21 +27,28 @@ public class ToolDamageHandler implements Listener {
     @EventHandler
     public void onBreak(BlockBreakEvent event) {
         ItemStack stack = event.getPlayer().getItemInHand();
-        if (stack != null) {
+        if (ItemUtils.isNotNully(stack)) {
             IEnergyProvider eProv = AdapterUtils.adapt(IEnergyProvider.class, stack);
-            if (eProv.requestEnergy((int)Math.ceil(Hardness.of(event.getBlock().getType()) * 150)) > 0)
-                damageCancels.put(event.getPlayer().getUniqueId(), Rail.currentTick());
+            if (eProv != null) {
+                if (eProv.requestEnergy((int)Math.ceil(Hardness.of(event.getBlock().getType()) * 150)) > 0)
+                    damageCancels.put(event.getPlayer().getUniqueId(), Rail.currentTick());
+            }
         }
     }
 
     @EventHandler
     public void onToolDamage(PlayerItemDamageEvent event) {
         Long cancelTime = damageCancels.remove(event.getPlayer().getUniqueId());
-        if (cancelTime != null && Rail.currentTick() - cancelTime <= 1)
+        if (cancelTime != null && Rail.currentTick() - cancelTime <= 1) {
             event.setCancelled(true);
-        IEnergyProvider eProv = AdapterUtils.adapt(IEnergyProvider.class, event.getItem());
-        if (eProv.requestEnergy(event.getDamage() * 200) > 0)
-            event.setCancelled(true);
+            event.getPlayer().updateInventory();
+        } else {
+            IEnergyProvider eProv = AdapterUtils.adapt(IEnergyProvider.class, event.getItem());
+            if (eProv != null && eProv.requestEnergy(event.getDamage() * 200) > 0) {
+                event.setCancelled(true);
+                event.getPlayer().updateInventory();
+            }
+        }
     }
 
 }
--- src/main/java/io/github/phantamanta44/mcrail/railtech/tool/ToolModule.java
+++ src/main/java/io/github/phantamanta44/mcrail/railtech/tool/ToolModule.java
@@ -1,12 +1,50 @@
 package io.github.phantamanta44.mcrail.railtech.tool;
 
+import io.github.phantamanta44.mcrail.Rail;
+import io.github.phantamanta44.mcrail.crafting.RailRecipe;
+import io.github.phantamanta44.mcrail.railflux.EnergyItem;
 import io.github.phantamanta44.mcrail.railtech.RTMain;
+import io.github.phantamanta44.mcrail.util.AdapterUtils;
+import io.github.phantamanta44.mcrail.util.ItemUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.function.Function;
 
 public class ToolModule {
 
     public static void init() {
         Bukkit.getServer().getPluginManager().registerEvents(new ToolDamageHandler(), RTMain.INSTANCE);
+        Rail.recipes().register(genRecipe(Material.DIAMOND_AXE, "Energized Diamond Axe", 128000));
+        Rail.recipes().register(genRecipe(Material.DIAMOND_PICKAXE, "Energized Diamond Pickaxe", 128000));
+        Rail.recipes().register(genRecipe(Material.DIAMOND_SWORD, "Energized Diamond Sword", 128000));
+        Rail.recipes().register(genRecipe(Material.DIAMOND_HOE, "Energized Diamond Hoe", 128000));
+        Rail.recipes().register(genRecipe(Material.DIAMOND_SPADE, "Energized Diamond Shovel", 128000));
+    }
+    
+    private static RailRecipe genRecipe(Material type, String name, int charge) {
+        return new RailRecipe()
+                .line("rrr").line("rtr").line("rpr")
+                .ingredient('r', Material.REDSTONE_BLOCK)
+                .ingredient('t', s -> ItemUtils.isNotNully(s)
+                        && s.getType() == type
+                        && AdapterUtils.adapt(EnergyItem.class, s) == null)
+                .ingredient('p', Material.PISTON_BASE)
+                .withResult(genResult(type, name, charge));
+    }
+    
+    private static Function<ItemStack[], ItemStack> genResult(Material type, String name, int charge) {
+        return mat -> {
+            ItemStack stack = new ItemStack(type, 1, mat[4].getDurability());
+            ItemMeta meta = stack.getItemMeta();
+            meta.setDisplayName(ChatColor.AQUA + name);
+            stack.setItemMeta(meta);
+            EnergyItem.energize(stack, charge, charge);
+            return stack;
+        };
     }
 
 }
