0d6052f1b2d71877155507b4adad8e11c592b0f9
phantamanta44 <evandalong@gmail.com>
2017-04-22 21:04:47 -0500

    Update to use custom items

---
+++ src/main/java/io/github/phantamanta44/mcrail/railtech/tool/ItemEnergizedTool.java
@@ -0,0 +1,55 @@
+package io.github.phantamanta44.mcrail.railtech.tool;
+
+import io.github.phantamanta44.mcrail.Rail;
+import io.github.phantamanta44.mcrail.item.characteristic.CharName;
+import io.github.phantamanta44.mcrail.railflux.item.ItemLastLoreEnergy;
+import io.github.phantamanta44.mcrail.railtech.util.Hardness;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ItemEnergizedTool extends ItemLastLoreEnergy {
+
+    private final Material material;
+    private final Map<UUID, Long> damageCancels;
+
+    public ItemEnergizedTool(Material material, String name, int charge) {
+        super(charge, charge);
+        this.material = material;
+        this.damageCancels = new ConcurrentHashMap<>();
+        this.characteristics.add(new CharName(ChatColor.AQUA + name));
+    }
+
+    @Override
+    public Material material() {
+        return material;
+    }
+
+    @Override
+    public void onDamage(PlayerItemDamageEvent event, ItemStack stack) {
+        Long cancelTime = damageCancels.remove(event.getPlayer().getUniqueId());
+        if (cancelTime != null && Rail.currentTick() - cancelTime <= 1) {
+            event.setCancelled(true);
+            event.getPlayer().updateInventory();
+        } else if (requestEnergy(event.getDamage() * 200, stack) > 0) {
+            event.setCancelled(true);
+            event.getPlayer().updateInventory();
+        }
+    }
+
+    @Override
+    public void onBlockBreak(BlockBreakEvent event, ItemStack stack) {
+        if (event.getPlayer().getGameMode() != GameMode.CREATIVE
+                && requestEnergy((int)Math.ceil(Hardness.of(event.getBlock().getType()) * 150), stack) > 0) {
+            damageCancels.put(event.getPlayer().getUniqueId(), Rail.currentTick());
+        }
+    }
+
+}
--- src/main/java/io/github/phantamanta44/mcrail/railtech/tool/ToolModule.java
+++ src/main/java/io/github/phantamanta44/mcrail/railtech/tool/ToolModule.java
@@ -2,49 +2,37 @@ package io.github.phantamanta44.mcrail.railtech.tool;
 
 import io.github.phantamanta44.mcrail.Rail;
 import io.github.phantamanta44.mcrail.crafting.RailRecipe;
-import io.github.phantamanta44.mcrail.railflux.EnergyItem;
-import io.github.phantamanta44.mcrail.railtech.RTMain;
-import io.github.phantamanta44.mcrail.util.AdapterUtils;
-import io.github.phantamanta44.mcrail.util.ItemUtils;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import java.util.function.Function;
 
 public class ToolModule {
 
     public static void init() {
-        Bukkit.getServer().getPluginManager().registerEvents(new ToolDamageHandler(), RTMain.INSTANCE);
-        Rail.recipes().register(genRecipe(Material.DIAMOND_AXE, "Energized Diamond Axe", 128000));
-        Rail.recipes().register(genRecipe(Material.DIAMOND_PICKAXE, "Energized Diamond Pickaxe", 128000));
-        Rail.recipes().register(genRecipe(Material.DIAMOND_SWORD, "Energized Diamond Sword", 128000));
-        Rail.recipes().register(genRecipe(Material.DIAMOND_HOE, "Energized Diamond Hoe", 128000));
-        Rail.recipes().register(genRecipe(Material.DIAMOND_SPADE, "Energized Diamond Shovel", 128000));
+        energyTool(Material.DIAMOND_PICKAXE, "ediapick", "Pickaxe", 250000);
+        energyTool(Material.DIAMOND_AXE, "ediaaxe", "Axe", 250000);
+        energyTool(Material.DIAMOND_HOE, "ediahoe", "Hoe", 250000);
+        energyTool(Material.DIAMOND_SWORD, "ediasword", "Sword", 250000);
+        energyTool(Material.DIAMOND_SPADE, "ediaspade", "Shovel", 250000);
+        energyTool(Material.DIAMOND_SPADE, "elighter", "Igniter", 25000);
+        energyTool(Material.DIAMOND_SPADE, "ebow", "Bow", 50000);
+        energyTool(Material.DIAMOND_SPADE, "erod", "Rod", 25000);
+        energyTool(Material.DIAMOND_SPADE, "eshears", "Shears", 50000);
     }
-    
-    private static RailRecipe genRecipe(Material type, String name, int charge) {
-        return new RailRecipe()
-                .line("rrr").line("rtr").line("rpr")
-                .ingredient('r', Material.REDSTONE_BLOCK)
-                .ingredient('t', s -> ItemUtils.isNotNully(s)
-                        && s.getType() == type
-                        && AdapterUtils.adapt(EnergyItem.class, s) == null)
-                .ingredient('p', Material.PISTON_BASE)
-                .withResult(genResult(type, name, charge));
-    }
-    
-    private static Function<ItemStack[], ItemStack> genResult(Material type, String name, int charge) {
-        return mat -> {
-            ItemStack stack = new ItemStack(type, 1, mat[4].getDurability());
-            ItemMeta meta = stack.getItemMeta();
-            meta.setDisplayName(ChatColor.AQUA + name);
-            stack.setItemMeta(meta);
-            EnergyItem.energize(stack, charge, charge);
-            return stack;
-        };
+
+    private static void energyTool(Material material, String id, String name, int charge) {
+        Rail.itemRegistry().register("railtech:" + id,
+                new ItemEnergizedTool(material, "Energized " + name, charge));
+        Rail.recipes().register(new RailRecipe()
+                .line("ata").line("apa").line(" o ")
+                .ingredient('a', Material.DIAMOND)
+                .ingredient('t', Material.REDSTONE_COMPARATOR)
+                .ingredient('p', material)
+                .ingredient('o', Material.OBSIDIAN)
+                .withResult(mat -> {
+                    ItemStack stack = Rail.itemRegistry().create("railtech:" + id);
+                    stack.setDurability(mat[4].getDurability());
+                    return stack;
+                }));
     }
 
 }
